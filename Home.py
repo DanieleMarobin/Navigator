d4131cc07a7d9a4b2f9978020994ff5dd1f6b45a
DanieleMarobin <daniele.marobin@hotmail.com>
2023-01-31 13:54:08 +0100

    Changed order

--- Home.py
+++ Home.py
@@ -12,7 +12,12 @@ import streamlit as st
 st.set_page_config(page_title="Navigator",layout="wide",initial_sidebar_state="expanded")
 
 st.markdown("# Navigator")
+
 st.markdown("---")
+st.markdown("### Price Analysis")
+
+link='Seasonals: [Seasonals](https://danielemarobin-seasonals-home.streamlit.app/)'
+st.markdown(link)
 
 link='Trade Radar: [Trade Radar](https://danielemarobin-traderadar-home.streamlit.app/)'
 st.markdown(link)
@@ -20,13 +25,22 @@ st.markdown(link)
 link='Price Models: [Price Models](https://danielemarobin-pricemodels-home.streamlit.app/)'
 st.markdown(link)
 
+
+st.markdown("---")
+st.markdown("### Yield Models")
+
+link='Safra Corn Yield Model: [Safra Corn Yield Model](https://danielemarobin-brasafracornyieldmodel-home.streamlit.app/)'
+st.markdown(link)
+
+
+st.markdown("---")
+st.markdown("### Crops")
+
 link='Crop Progress and Conditions: [Crop Progress and Conditions](https://danielemarobin-cropprogress-home.streamlit.app/)'
 st.markdown(link)
 
 # link='Trade Flow: [Trade Flow](https://danielemarobin-tradeflow-home.streamlit.app/)'
 # st.markdown(link)
 
-link='Safra Corn Yield Model: [Safra Corn Yield Model](https://danielemarobin-brasafracornyieldmodel-home.streamlit.app/)'
-st.markdown(link)
 
 # st.sidebar.markdown("# Navigator")
